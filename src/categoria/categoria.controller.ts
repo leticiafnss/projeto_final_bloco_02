1c459a7f9ca8e83b92b7af10f4223f91bdf104f3
leticiafnss <leticiafnss@gmail.com>
2026-04-16 13:16:01 -0300

    feat: implementa CRUD completo de categoria

---
+++ src/categoria/categoria.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CategoriaService } from './categoria.service';
+import { Categoria } from './entities/categoria.entity';
+
+@Controller('/categorias')
+export class CategoriaController {
+  constructor(private readonly categoriaService: CategoriaService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() categoria: Categoria): Promise<Categoria> {
+    return this.categoriaService.create(categoria);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Categoria[]> {
+    return this.categoriaService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Categoria> {
+    return this.categoriaService.findById(id);
+  }
+
+  @Get('/descricao/:descricao')
+  @HttpCode(HttpStatus.OK)
+  findByDescricao(@Param('descricao') descricao: string): Promise<Categoria[]> {
+    return this.categoriaService.findByDescricao(descricao);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() categoria: Categoria): Promise<Categoria> {
+    return this.categoriaService.update(categoria);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriaService.delete(id);
+  }
+}
